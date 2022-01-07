9e778bb9dcf7d44a85d2665c0eaa787bb93bbb7d
kurudrive <kurudrive@gmail.com>
2022-01-07 19:28:32 +0900

    css仮読み込み

---
+++ functions.php
@@ -0,0 +1,23 @@
+<?php
+
+if ( ! function_exists( 'x29_support' ) ) :
+	function x29_support() {
+
+		// Adding support for core block visual styles.
+		add_theme_support( 'wp-block-styles' );
+
+		// Enqueue editor styles.
+		add_editor_style( 'assets/css/editor-style.css' );
+	}
+	add_action( 'after_setup_theme', 'x29_support' );
+endif;
+
+/**
+ * Enqueue scripts and styles.
+ */
+function x29_scripts() {
+	// Enqueue theme stylesheet.
+	wp_enqueue_style( 'x29-style', get_template_directory_uri() . '/assets/css/style.css', array(), wp_get_theme()->get( 'Version' ) );
+}
+
+add_action( 'wp_enqueue_scripts', 'x29_scripts' );
