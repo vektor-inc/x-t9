f0bd75dd4b318ade9759842d4cc547f6521a63b0
kurudrive <kurudrive@gmail.com>
2022-01-16 01:55:47 +0900

    blankテンプレート追加など

--- functions.php
+++ functions.php
@@ -28,7 +28,7 @@ add_action( 'wp_enqueue_scripts', 'x29_scripts' );
  *
  * @return string archive title
  */
-function lightning_get_the_archive_title() {
+function x29_get_the_archive_title() {
 	$title = '';
 	if ( is_category() ) {
 		$title = single_cat_title( '', false );
@@ -37,11 +37,11 @@ function lightning_get_the_archive_title() {
 	} elseif ( is_author() ) {
 		$title = get_the_author();
 	} elseif ( is_year() ) {
-		$title = get_the_date( _x( 'Y', 'yearly archives date format', 'lightning' ) );
+		$title = get_the_date( _x( 'Y', 'yearly archives date format', 'x29' ) );
 	} elseif ( is_month() ) {
-		$title = get_the_date( _x( 'F Y', 'monthly archives date format', 'lightning' ) );
+		$title = get_the_date( _x( 'F Y', 'monthly archives date format', 'x29' ) );
 	} elseif ( is_day() ) {
-		$title = get_the_date( _x( 'F j, Y', 'daily archives date format', 'lightning' ) );
+		$title = get_the_date( _x( 'F j, Y', 'daily archives date format', 'x29' ) );
 	} elseif ( is_post_type_archive() ) {
 		$title = post_type_archive_title( '', false );
 	} elseif ( is_tax() ) {
@@ -60,9 +60,9 @@ function lightning_get_the_archive_title() {
 		if ( $post_type ) {
 			$title = get_post_type_object( $post_type )->labels->name;
 		} else {
-			$title = __( 'Archives', 'lightning' );
+			$title = __( 'Archives', 'x29' );
 		}
 	}
-	return apply_filters( 'lightning_get_the_archive_title', $title );
+	return apply_filters( 'x29_get_the_archive_title', $title );
 }
-add_filter( 'get_the_archive_title', 'lightning_get_the_archive_title' );
+add_filter( 'get_the_archive_title', 'x29_get_the_archive_title' );
