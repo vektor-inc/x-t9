e8516f68ad6c1eb7d1bcbc16233cdde0e2acd1cf
kurudrive <kurudrive@gmail.com>
2022-02-23 11:17:25 +0900

    [ Design bug fix ] Fix content width corruption

--- inc/patterns/hidden-404.php
+++ inc/patterns/hidden-404.php
@@ -5,10 +5,15 @@
 return array(
 	'title'    => __( '404 content', 'x-t9' ),
 	'inserter' => false,
-	'content'  => '<!-- wp:heading {"textAlign":"center","level":1,"align":"full","backgroundColor":"bg-light-gray"} -->
-	<h1 class="alignfull has-text-align-center has-bg-light-gray-background-color has-background" id="404-not-found">' . esc_html( _x( '404', 'Error code for a webpage that is not found.', 'x-t9' ) ) . '</h1>
-					<!-- /wp:heading -->
-					<!-- wp:spacer -->
+	'content'  => '<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}},"backgroundColor":"bg-light-gray","className":"page-header"} -->
+					<div class="wp-block-group alignfull page-header has-bg-light-gray-background-color has-background" style="padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><!-- wp:group {"layout":{"inherit":true}} -->
+					<div class="wp-block-group"><!-- wp:heading {"textAlign":"center","level":1,"style":{"spacing":{"margin":{"top":"1.25em","right":"0em","bottom":"1.25em","left":"0em"}}},"backgroundColor":"bg-light-gray"} -->
+					<h1 class="has-text-align-center has-bg-light-gray-background-color has-background" id="404-not-found" style="margin-top:1.25em;margin-right:0em;margin-bottom:1.25em;margin-left:0em">' . esc_html( _x( '404', 'Error code for a webpage that is not found.', 'x-t9' ) ) . '</h1>
+					<!-- /wp:heading --></div>
+					<!-- /wp:group --></div>
+					<!-- /wp:group -->
+
+					<!-- wp:spacer {"height":80} -->
 					<div style="height:80px" aria-hidden="true" class="wp-block-spacer"></div>
 					<!-- /wp:spacer -->
 					<!-- wp:paragraph {"align":"center"} -->
--- inc/patterns/query-image-left.php
+++ inc/patterns/query-image-left.php
@@ -9,7 +9,7 @@ return array(
 	'title'      => __( 'X-T9 Query image at left', 'x-t9' ),
 	'categories' => array( 'query' ),
 	'blockTypes' => array( 'core/query' ),
-	'content'    => '<!-- wp:query {"query":{"perPage":"10","pages":"0","offset":0,"postType":"post","categoryIds":[],"tagIds":[],"order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true},"displayLayout":{"type":"list","columns":3},"layout":{"inherit":true}} -->
+	'content'    => '<!-- wp:query {"query":{"perPage":"10","pages":"0","offset":0,"postType":"post","categoryIds":[],"tagIds":[],"order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true},"displayLayout":{"type":"list","columns":3},"layout":{"inherit":false}} -->
 	<div class="wp-block-query"><!-- wp:pattern {"slug":"x-t9/post-template-image-left"} /-->	
 	
 	<!-- wp:query-pagination {"paginationArrow":"chevron","layout":{"type":"flex","justifyContent":"center","orientation":"horizontal","flexWrap":"wrap"}} -->
