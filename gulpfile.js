b187513585b6a00d562fa24b26d0cecfeece6827
kurudrive <kurudrive@gmail.com>
2022-02-25 11:48:44 +0900

    [ dev ] dist command improvement

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+
+gulp.task('dist', function() {
+	return gulp.src(
+			[
+				'./**/*.php',
+				'./**/*.txt',
+				'./**/*.css',
+				'./**/*.scss',
+				'./**/*.bat',
+				'./**/*.rb',
+				'./**/*.eot',
+				'./**/*.svg',
+				'./**/*.ttf',
+				'./**/*.woff',
+				'./**/*.woff2',
+				'./**/*.otf',
+				'./**/*.less',
+				'./**/*.png',
+				'./assets/**',
+				'./inc/**',
+				'./parts/**',
+				'./templates/**',
+				"!./tests/**",
+				"!./dist/**",
+				"!./node_modules/**"
+			],
+			{ base: './' }
+		)
+		.pipe( gulp.dest( 'dist/x-t9' ) );
+} );
