ef34759133f1ffba57e967db0e15649f59ed5589
kurudrive <kurudrive@gmail.com>
2022-07-02 16:01:21 +0900

    [ Design Improvement ] Header pattern update

--- inc/patterns/header-logo-nav-float-contact-top.php
+++ inc/patterns/header-logo-nav-float-contact-top.php
@@ -14,8 +14,8 @@ return array(
 	<div class="wp-block-group"><!-- wp:site-tagline {"textColor":"text-bright","fontSize":"x-small"} /-->
 	
 	<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"right"}} -->
-	<div class="wp-block-buttons"><!-- wp:button {"style":{"border":{"radius":"0px"},"spacing":{"padding":{"top":"0.4rem","bottom":"0.4rem","left":"2rem","right":"2rem"}}},"fontSize":"small"} -->
-	<div class="wp-block-button has-custom-font-size has-small-font-size"><a class="wp-block-button__link" style="border-radius:0px;padding-top:0.4rem;padding-right:2rem;padding-bottom:0.4rem;padding-left:2rem">Contact</a></div>
+	<div class="wp-block-buttons"><!-- wp:button {"style":{"border":{"radius":"0px"},"spacing":{"padding":{"top":"0.2rem","bottom":"0.2rem","left":"2rem","right":"2rem"}}},"fontSize":"small"} -->
+	<div class="wp-block-button has-custom-font-size has-small-font-size"><a class="wp-block-button__link" style="border-radius:0px;padding-top:0.2rem;padding-right:2rem;padding-bottom:0.2rem;padding-left:2rem">Contact</a></div>
 	<!-- /wp:button --></div>
 	<!-- /wp:buttons --></div>
 	<!-- /wp:group -->
@@ -26,12 +26,12 @@ return array(
 	<!-- /wp:group -->
 	
 	<!-- wp:group {"style":{"spacing":{"padding":{"top":"5px","bottom":"5px"}}}} -->
-	<div class="wp-block-group" style="padding-top:5px;padding-bottom:5px"><!-- wp:navigation {"layout":{"type":"flex","justifyContent":"left","orientation":"horizontal","flexWrap":"wrap"}} /--></div>
+	<div class="wp-block-group" style="padding-top:5px;padding-bottom:5px"><!-- wp:navigation {"className":"nav\u002d\u002dactive-border-bottom","layout":{"type":"flex","justifyContent":"left","orientation":"horizontal","flexWrap":"wrap"}} /--></div>
 	<!-- /wp:group --></div>
 	<!-- /wp:group --></div>
 	<!-- /wp:group -->
 	
-	<!-- wp:separator {"color":"border-light","className":"is-style-wide"} -->
-	<hr class="wp-block-separator has-text-color has-background has-border-light-background-color has-border-light-color is-style-wide"/>
+	<!-- wp:separator {"opacity":"css","backgroundColor":"border-light","className":"is-style-wide"} -->
+	<hr class="wp-block-separator has-text-color has-border-light-color has-css-opacity has-border-light-background-color has-background is-style-wide"/>
 	<!-- /wp:separator -->',
 );
