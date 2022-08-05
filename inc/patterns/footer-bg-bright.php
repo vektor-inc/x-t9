8ac6d9a34757153dd7753ec201f5877926d11ba2
kurudrive <kurudrive@gmail.com>
2022-08-05 14:24:53 +0900

    [ デザイン調整 ] フッター BG Bright

--- inc/patterns/footer-bg-bright.php
+++ inc/patterns/footer-bg-bright.php
@@ -15,10 +15,16 @@ return array(
 <!-- /wp:separator -->
 
 <!-- wp:group {"backgroundColor":"bg-light-gray","className":"has-text-color"} -->
-<div class="wp-block-group has-text-color has-bg-light-gray-background-color has-background"><!-- wp:group {"style":{"spacing":{"padding":{"top":"2rem","bottom":"2rem"}}},"layout":{"inherit":true}} -->
-<div class="wp-block-group" style="padding-top:2rem;padding-bottom:2rem"><!-- wp:image {"align":"center","id":2623,"width":180,"height":45,"sizeSlug":"full","linkDestination":"media"} -->
-<figure class="wp-block-image aligncenter size-full is-resized"><a href="http://xt9dev.local/wp-content/uploads/2021/01/head-logo-bk.png"><img src="http://xt9dev.local/wp-content/uploads/2021/01/head-logo-bk.png" alt="" class="wp-image-2623" width="180" height="45"/></a></figure>
-<!-- /wp:image -->
+<div class="wp-block-group has-text-color has-bg-light-gray-background-color has-background"><!-- wp:group {"layout":{"inherit":true}} -->
+<div class="wp-block-group"><!-- wp:spacer {"className":"is-style-spacer-lg"} -->
+<div style="height:100px" aria-hidden="true" class="wp-block-spacer is-style-spacer-lg"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:site-logo {"width":200,"align":"center"} /-->
+
+<!-- wp:spacer {"height":"22px","className":"is-style-spacer-md"} -->
+<div style="height:22px" aria-hidden="true" class="wp-block-spacer is-style-spacer-md"></div>
+<!-- /wp:spacer -->
 
 <!-- wp:social-links {"iconColor":"text-secondary","iconColorValue":"rgba(0,0,0,0.5)","size":"has-normal-icon-size","className":"is-style-logos-only","layout":{"type":"flex","justifyContent":"center"},"style":{"spacing":{"blockGap":"1rem"}}} -->
 <ul class="wp-block-social-links has-normal-icon-size has-icon-color is-style-logos-only"><!-- wp:social-link {"url":"https://wordpress.org/","service":"facebook"} /-->
@@ -28,7 +34,11 @@ return array(
 <!-- wp:social-link {"url":"https://wordpress.org/","service":"instagram"} /-->
 
 <!-- wp:social-link {"url":"https://wordpress.org/","service":"youtube"} /--></ul>
-<!-- /wp:social-links --></div>
+<!-- /wp:social-links -->
+
+<!-- wp:spacer {"height":"22px","className":"is-style-spacer-lg"} -->
+<div style="height:22px" aria-hidden="true" class="wp-block-spacer is-style-spacer-lg"></div>
+<!-- /wp:spacer --></div>
 <!-- /wp:group -->
 
 <!-- wp:group {"className":"mt-0","layout":{"inherit":true}} -->
@@ -36,8 +46,8 @@ return array(
 <hr class="wp-block-separator has-text-color has-border-normal-color has-alpha-channel-opacity has-border-normal-background-color has-background is-style-wide"/>
 <!-- /wp:separator -->
 
-<!-- wp:group {"style":{"spacing":{"padding":{"top":"0.5rem","bottom":"0.5rem"}}},"layout":{"type":"flex","allowOrientation":false,"justifyContent":"space-between"}} -->
-<div class="wp-block-group" style="padding-top:0.5rem;padding-bottom:0.5rem"><!-- wp:navigation {"fontSize":"x-small"} /-->
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"0.5rem","bottom":"0.5rem"}}},"textColor":"text-secondary","layout":{"type":"flex","allowOrientation":false,"justifyContent":"space-between"}} -->
+<div class="wp-block-group has-text-secondary-color has-text-color" style="padding-top:0.5rem;padding-bottom:0.5rem"><!-- wp:navigation {"className":"is-style-nav\u002d\u002dtext-inline","fontSize":"x-small"} /-->
 
 <!-- wp:paragraph {"align":"center","style":{"typography":{"textTransform":"uppercase"}},"fontSize":"x-small"} -->
 <p class="has-text-align-center has-x-small-font-size" style="text-transform:uppercase">Copyright (C) X-T9 All Rights Reserved.</p>
