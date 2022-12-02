2145b5934def0f197dfd81cb0c3ce46e748f8182
kurudrive <kurudrive@gmail.com>
2022-12-03 00:27:45 +0900

    パターンに含混入した固有のナビ id 削除

--- inc/patterns/footer/footer-bg-secondary-center-nav.php
+++ inc/patterns/footer/footer-bg-secondary-center-nav.php
@@ -29,7 +29,7 @@ return array(
 	<div style="height:100px" aria-hidden="true" class="wp-block-spacer is-style-spacer-xs"></div>
 	<!-- /wp:spacer -->
 	
-	<!-- wp:navigation {"ref":11229,"showSubmenuIcon":false,"className":"is-style-nav\u002d\u002dtext-inline","layout":{"type":"flex","justifyContent":"center"},"fontSize":"x-small"} /-->
+	<!-- wp:navigation {"showSubmenuIcon":false,"className":"is-style-nav\u002d\u002dtext-inline","layout":{"type":"flex","justifyContent":"center"},"fontSize":"x-small"} /-->
 	
 	<!-- wp:social-links {"iconColor":"text-secondary","iconColorValue":"rgba(0,0,0,0.5)","size":"has-normal-icon-size","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|40","left":"var:preset|spacing|40"},"margin":{"top":"var:preset|spacing|40","right":"0","bottom":"0","left":"0"}}},"className":"is-style-logos-only","layout":{"type":"flex","justifyContent":"center"}} -->
 	<ul class="wp-block-social-links has-normal-icon-size has-icon-color is-style-logos-only" style="margin-top:var(--wp--preset--spacing--40);margin-right:0;margin-bottom:0;margin-left:0"><!-- wp:social-link {"url":"https://wordpress.org/","service":"facebook"} /-->
