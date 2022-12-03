81c0abb3d3f3e6830a726182936d868901da8ebe
kurudrive <kurudrive@gmail.com>
2022-12-03 15:01:29 +0900

    1.2.2 [ Other ] Translation adjustment

--- inc/patterns/hidden-404.php
+++ inc/patterns/hidden-404.php
@@ -6,13 +6,11 @@ return array(
 	'title'      => __( '404 Content', 'x-t9' ),
 	'inserter'   => false,
 	'categories' => array( 'pages' ),
-	'content'    => '<!-- wp:spacer {"height":80} -->
-					<div style="height:80px" aria-hidden="true" class="wp-block-spacer"></div>
-					<!-- /wp:spacer -->
-					<!-- wp:paragraph {"align":"center"} -->
+	'content'    => '<!-- wp:paragraph {"align":"center"} -->
 					<p class="has-text-align-center">' . esc_html__( 'This page could not be found. Maybe try a search?', 'x-t9' ) . '</p>
 					<!-- /wp:paragraph -->
-					<!-- wp:search {"label":"Search","showLabel":false,"width":50,"widthUnit":"%","buttonText":"Search","buttonUseIcon":true,"align":"center"} /--><!-- wp:spacer -->
-					<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
-					<!-- /wp:spacer -->',
+					<!-- wp:spacer {"className":"is-style-spacer-sm"} -->
+					<div style="height:100px" aria-hidden="true" class="wp-block-spacer is-style-spacer-sm"></div>
+					<!-- /wp:spacer -->
+					<!-- wp:search {"label":"Search","showLabel":false,"width":50,"widthUnit":"%","buttonText":"Search","buttonUseIcon":true,"align":"center"} /-->',
 );
