da6aac9a1b2524d0da6f6a057e9ed62cec5d8614
kurudrive <kurudrive@gmail.com>
2026-05-29 22:47:46 +0900

    パターンファイル構成変更

---
+++ patterns/header-sticky___logo--nav-contact.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Title: Sticky | Logo -- Nav - Contact
+ * Slug: x-t9/header/sticky___logo--nav-contact
+ * Categories: header
+ * Block Types: core/template-part/header
+ * Description:
+ *
+ * @package WordPress
+ * @subpackage x-t9
+ * @since X-T9 1.39.0
+ */
+
+?>
+
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"left":"0rem","right":"0rem"}},"position":{"type":"sticky","top":"0px"}},"backgroundColor":"bg-primary","layout":{"inherit":true,"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-bg-primary-background-color has-background" style="padding-right:0rem;padding-left:0rem"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group"><!-- wp:site-logo {"width":160,"className":"auto-size"} /-->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"0","bottom":"0"}}},"layout":{"type":"flex","allowOrientation":false,"flexWrap":"nowrap","justifyContent":"left"}} -->
+<div class="wp-block-group site-logo--set" style="padding-top:0;padding-bottom:0"><!-- wp:navigation {"overlay":"navigation-overlay","icon":"menu","className":"nav\u002d\u002dopen\u002d\u002dlg-up nav\u002d\u002dfirst-nowrap is-style-nav\u002d\u002dactive-border-bottom","style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"fontSize":"x-small","layout":{"type":"flex","justifyContent":"right","orientation":"horizontal","flexWrap":"nowrap"}} /-->
+
+<!-- wp:buttons {"style":{"spacing":{"margin":{"top":"0px","bottom":"0px"}}},"layout":{"type":"flex","justifyContent":"right"}} -->
+<div class="wp-block-buttons" style="margin-top:0px;margin-bottom:0px"><!-- wp:button {"backgroundColor":"primary","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}},"typography":{"lineHeight":"1"}},"fontSize":"small"} -->
+<div class="wp-block-button"><a class="wp-block-button__link has-primary-background-color has-background has-small-font-size has-custom-font-size wp-element-button" style="padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);line-height:1">Contact</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:separator {"align":"full","className":"is-style-wide","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}},"backgroundColor":"border-normal"} -->
+<hr class="wp-block-separator alignfull has-text-color has-border-normal-color has-alpha-channel-opacity has-border-normal-background-color has-background is-style-wide" style="margin-top:0;margin-bottom:0"/>
+<!-- /wp:separator --></div>
+<!-- /wp:group -->
+ 
